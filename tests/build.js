256409187e427c64d49992978bf4516b1c1aa845
DannyMcwaves <dannymcwaves96@rocketmail.com>
2016-12-14 11:55:40 +0000

    provides the build command for transpling to es5

---
+++ tests/build.js
@@ -0,0 +1,19 @@
+
+var watch = require("node-watch"),
+    command = require("child_process").exec;
+
+(() => {
+    let to_watch = ""
+    if (process.argv.length <= 2) {
+        console.log("please specify a directory or file to build");
+        return null;
+    } else {
+        to_watch = process.argv.pop();
+    }
+
+    console.log(to_watch);
+    var arg = `node_modules/.bin/babel ${to_watch} -o es5/compiled_es6.js`;
+    console.log(arg);
+    command(arg, (err, stdout, stderr) => {console.log(err || stdout || stderr)})
+
+})();
