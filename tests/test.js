57c6318e15bdfe241752f4c38de84b842c6b35da
DannyMcwaves <dannymcwaves96@rocketmail.com>
2016-12-14 11:56:06 +0000

    provides the test command for testing the sample codes

---
+++ tests/test.js
@@ -0,0 +1,19 @@
+
+var watch = require("node-watch"),
+    command = require("child_process").exec;
+
+(() => {
+    let to_watch = ""
+    if (process.argv.length <= 2) {
+        console.log("please specify a directory to watch and run files on changes");
+        return null;
+    } else {
+        to_watch = process.argv.pop() + "/";
+    }
+
+    watch(to_watch, (filename) =>  {
+        var arg = `node_modules/.bin/babel-node ${filename}`;
+        command(arg, (err, stdout, stderr) => {console.log(err || stdout || stderr)})
+    });
+
+})()
